a722dbf79fdbcaf1d397c19194df4a8e73499f24
Ediprofe <elprofeedi@gmail.com>
2025-10-10 16:29:18 -0500

    47

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -59,7 +59,7 @@ export default function FileUploader({ onFileLoaded }) {
         <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
           <iframe
             className="absolute top-0 left-0 w-full h-full rounded-lg shadow-md"
-            src="https://www.youtube.com/embed/cIvXPRVTTyQ"
+            src="https://www.youtube.com/embed/IvSn37VhKOY"
             title="Video de demostración - Analizador ICFES"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
